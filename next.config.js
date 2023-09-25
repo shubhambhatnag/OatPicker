60e7bf0a9a9f041686c0db0b6cc799eab62d0700
Shubham Bhatnagar <shubhamb@terpmail.umd.edu>
2023-09-24 21:34:37 -0400

    hello

--- next.config.js
+++ next.config.js
@@ -2,3 +2,9 @@
 const nextConfig = {}
 
 module.exports = nextConfig
+reactStrictMode = false
+
+module.exports = {
+    distDir: 'out', // Make sure this matches your actual output directory
+    // ... other configurations ...
+  };
